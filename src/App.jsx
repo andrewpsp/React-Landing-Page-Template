c61909abf0c8d98ca3abeef18c8c56c2b3a9ca13
Andrew Thompson <drewpsp@gmail.com>
2023-04-02 23:43:47 -0400

    Small update for readability Update App.jsx
    Consolidate components imports into a single line from the newly created index.js Export all the files instead for readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
+import { Navigation, Header, Features, About, Services, Gallery, Testimonials, Team, Contact, Navigation } from "./components";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
