4201ad710e53fd6a9477903a6d24b3655eba30b0
Andrew Thompson <drewpsp@gmail.com>
2023-04-02 23:36:59 -0400

    Create index.js within Components folder
    Small change to Clean the main js file from cluttered imported by adding a index.js and importing a one-liner instead

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import  Navigation from "./navigation";
+import  Header  from "./header";
+import  Features  from "./features";
+import  About  from "./about";
+import  Services  from "./services";
+import  Gallery  from "./gallery";
+import  Testimonials  from "./testimonials";
+import  Team  from "./Team";
+import  Contact  from "./contact";
+
+export {
+    Navigation,
+    Header,
+    Features,
+    About,
+    Services,
+    Gallery,
+    Testimonials,
+    Team,
+    Contact,
+};
